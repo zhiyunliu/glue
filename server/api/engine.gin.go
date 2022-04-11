c53bc3189340c0add51d0696cc4be51eb46e5330
liujinyin <liujinyin@xunyou.com>
2022-04-11 01:34:48 -0700

    add the health check

--- server/api/engine.gin.go
+++ server/api/engine.gin.go
@@ -1,12 +1,15 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/zhiyunliu/gel/global"
 	"github.com/zhiyunliu/gel/server"
 )
 
 func (e *Server) registryEngineRoute() {
-	gin.SetMode("release")
+	gin.SetMode(global.Mode)
 	engine := gin.New()
 	e.opts.handler = engine
 	adapterEngine := server.NewGinEngine(engine,
@@ -15,8 +18,9 @@ func (e *Server) registryEngineRoute() {
 		server.WithRequestDecoder(e.opts.decReq),
 		server.WithResponseEncoder(e.opts.encResp))
 
-	engine.NoMethod()
-	engine.NoRoute()
+	engine.Handle(http.MethodGet, "/healthcheck", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "success")
+	})
 
 	server.RegistryEngineRoute(adapterEngine, e.opts.router)
 }
