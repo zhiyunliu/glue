9b55ef069f3addfe2ada8fd4de30d423cb96f67c
liujinyin <liujinyin@xunyou.com>
2022-07-11 17:27:32 +0800

    修改日志中间件的 错误 打印，增加 请求body .

--- constants/content.type.go
+++ constants/content.type.go
@@ -4,4 +4,5 @@ const (
 	ContentTypeName            = "Content-Type"
 	ContentTypeApplicationJSON = "application/json;charset=utf-8"
 	ContentTypeTextPlain       = "text/plain;charset=utf-8"
+	ContentTypeUrlencoded      = "application/x-www-form-urlencoded;charset=utf-8"
 )
--- middleware/logging/logging.go
+++ middleware/logging/logging.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/zhiyunliu/glue/context"
 	"github.com/zhiyunliu/glue/log"
+	"github.com/zhiyunliu/golibs/bytesconv"
 
 	"github.com/zhiyunliu/glue/errors"
 	"github.com/zhiyunliu/glue/middleware"
@@ -35,9 +36,9 @@ func Server(logger log.Logger) middleware.Middleware {
 				code = se.Code
 			}
 
-			level, stack := extractError(err)
+			level, errInfo := extractError(err)
 			if level == log.LevelError {
-				ctx.Log().Logf(level, "%s.resp %s %s %d %s %s", kind, ctx.Request().GetMethod(), fullPath, code, time.Since(startTime).String(), stack)
+				ctx.Log().Logf(level, "%s.resp %s %s %d %s %s %s", kind, ctx.Request().GetMethod(), fullPath, code, time.Since(startTime).String(), extractBody(ctx.Request()), errInfo)
 			} else {
 				ctx.Log().Logf(level, "%s.resp %s %s %d %s ", kind, ctx.Request().GetMethod(), fullPath, code, time.Since(startTime).String())
 			}
@@ -53,11 +54,14 @@ func extractReq(req context.Request) string {
 		return req.Query().String()
 	}
 	return ""
-	// if req.Body().Len() > 0 {
-	// 	result[1] = bytesconv.BytesToString(req.Body().Bytes())
-	// }
+}
 
-	//return strings.Join(result, " ")
+// extractArgs returns the string of the req
+func extractBody(req context.Request) string {
+	if req.Body().Len() > 0 {
+		return bytesconv.BytesToString(req.Body().Bytes())
+	}
+	return ""
 }
 
 func extractResp(resp interface{}) string {
@@ -70,7 +74,7 @@ func extractResp(resp interface{}) string {
 // extractError returns the string of the error
 func extractError(err error) (log.Level, string) {
 	if err != nil {
-		return log.LevelError, fmt.Sprintf("%+v", err)
+		return log.LevelError, err.Error()
 	}
 	return log.LevelInfo, ""
 }
