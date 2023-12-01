c1f8ed0cccc5e52c8780eafd1cca37a5c5f1a024
liujinyin <liujinyin@xunyou.com>
2023-12-01 10:22:47 +0800

    1. 处理gorm的接口实现支持

--- contrib/xdb/trans.go
+++ contrib/xdb/trans.go
@@ -2,12 +2,12 @@ package xdb
 
 import (
 	"context"
+	"database/sql"
 	"time"
 
 	"github.com/zhiyunliu/glue/contrib/xdb/internal"
 	"github.com/zhiyunliu/glue/contrib/xdb/tpl"
 	"github.com/zhiyunliu/glue/xdb"
-	"github.com/zhiyunliu/golibs/xtypes"
 )
 
 // xTrans 数据库事务操作类
@@ -18,21 +18,50 @@ type xTrans struct {
 }
 
 // Query 查询数据
-func (db *xTrans) Query(ctx context.Context, sql string, input map[string]interface{}) (rows xdb.Rows, err error) {
-	start := time.Now()
-	query, execArgs := db.tpl.GetSQLContext(sql, input)
+func (db *xTrans) Query(ctx context.Context, sqls string, input any) (rows xdb.Rows, err error) {
+	tmp, err := db.dbQuery(ctx, sqls, input, func(r *sql.Rows) (any, error) {
+		return internal.ResolveRows(r)
+	})
+	rows = tmp.(xdb.Rows)
+	return
+}
 
-	debugPrint(ctx, db.cfg, query, execArgs...)
-	data, err := db.tx.Query(query, execArgs...)
+// Query 查询数据
+func (db *xTrans) Multi(ctx context.Context, sqls string, input any) (datasetRows []xdb.Rows, err error) {
+	tmp, err := db.dbQuery(ctx, sqls, input, func(r *sql.Rows) (any, error) {
+		return internal.ResolveMultiRows(r)
+	})
+	datasetRows = tmp.([]xdb.Rows)
+	return
+}
+
+func (t *xTrans) First(ctx context.Context, sqls string, input any) (data xdb.Row, err error) {
+	tmp, err := t.dbQuery(ctx, sqls, input, func(r *sql.Rows) (any, error) {
+		return internal.ResolveFirstRow(r)
+	})
+	data = tmp.(xdb.Row)
+	return
+}
+
+// Scalar 根据包含@名称占位符的查询语句执行查询语句
+func (t *xTrans) Scalar(ctx context.Context, sqls string, input any) (data interface{}, err error) {
+	data, err = t.dbQuery(ctx, sqls, input, func(r *sql.Rows) (any, error) {
+		return internal.ResolveScalar(r)
+	})
+	return
+}
+
+// Execute 根据包含@名称占位符的语句执行查询语句
+func (db *xTrans) Exec(ctx context.Context, sql string, input any) (r xdb.Result, err error) {
+
+	dbParams, err := internal.ResolveParams(input)
 	if err != nil {
-		return nil, internal.GetError(err, query, execArgs...)
+		return
 	}
-	defer func() {
-		if data != nil {
-			data.Close()
-		}
-	}()
-	rows, err = internal.ResolveRows(data)
+	query, execArgs := db.tpl.GetSQLContext(sql, dbParams)
+	start := time.Now()
+	debugPrint(ctx, db.cfg, query, execArgs...)
+	r, err = db.tx.Execute(query, execArgs...)
 	if err != nil {
 		return nil, internal.GetError(err, query, execArgs...)
 	}
@@ -41,68 +70,16 @@ func (db *xTrans) Query(ctx context.Context, sql string, input map[string]interf
 }
 
 // Query 查询数据
-func (db *xTrans) Multi(ctx context.Context, sql string, input map[string]interface{}) (datasetRows []xdb.Rows, err error) {
-	start := time.Now()
-
-	query, execArgs := db.tpl.GetSQLContext(sql, input)
-
-	debugPrint(ctx, db.cfg, query, execArgs...)
-
-	sqlRows, err := db.tx.Query(query, execArgs...)
-	if err != nil {
-		return nil, internal.GetError(err, query, execArgs...)
-	}
-	defer func() {
-		if sqlRows != nil {
-			sqlRows.Close()
-		}
-	}()
-	datasetRows, err = internal.ResolveMultiRows(sqlRows)
-	if err != nil {
-		return nil, internal.GetError(err, query, execArgs...)
-	}
-	printSlowQuery(ctx, db.cfg, time.Since(start), query, execArgs...)
-	return
+func (db *xTrans) QueryAs(ctx context.Context, sqls string, input any, results any) (err error) {
+	return db.dbQueryAs(ctx, sqls, input, results, func(r *sql.Rows, a any) error {
+		return internal.ResolveDataResult(r, results)
+	})
 }
 
-func (t *xTrans) First(ctx context.Context, sql string, input map[string]interface{}) (data xdb.Row, err error) {
-	rows, err := t.Query(ctx, sql, input)
-	if err != nil {
-		return
-	}
-	if rows.IsEmpty() {
-		data = make(xtypes.XMap)
-		return
-	}
-	data = rows[0]
-	return
-}
-
-// Scalar 根据包含@名称占位符的查询语句执行查询语句
-func (t *xTrans) Scalar(ctx context.Context, sql string, input map[string]interface{}) (result interface{}, err error) {
-	rows, err := t.Query(ctx, sql, input)
-	if err != nil {
-		return
-	}
-	if rows.Len() == 0 || len(rows[0]) == 0 {
-		return nil, nil
-	}
-	result, _ = rows[0].Get(rows[0].Keys()[0])
-	return
-}
-
-// Execute 根据包含@名称占位符的语句执行查询语句
-func (db *xTrans) Exec(ctx context.Context, sql string, input map[string]interface{}) (r xdb.Result, err error) {
-	start := time.Now()
-	query, execArgs := db.tpl.GetSQLContext(sql, input)
-
-	debugPrint(ctx, db.cfg, query, execArgs...)
-	r, err = db.tx.Execute(query, execArgs...)
-	if err != nil {
-		return nil, internal.GetError(err, query, execArgs...)
-	}
-	printSlowQuery(ctx, db.cfg, time.Since(start), query, execArgs...)
-	return
+func (db *xTrans) FirstAs(ctx context.Context, sqls string, input any, result any) (err error) {
+	return db.dbQueryAs(ctx, sqls, input, result, func(r *sql.Rows, a any) error {
+		return internal.ResolveDataResult(r, result)
+	})
 }
 
 // Rollback 回滚所有操作
@@ -113,4 +90,54 @@ func (t *xTrans) Rollback() error {
 // Commit 提交所有操作
 func (t *xTrans) Commit() error {
 	return t.tx.Commit()
+}
+
+func (db *xTrans) dbQuery(ctx context.Context, sql string, input any, callback internal.DbResolveMapValCallback) (result any, err error) {
+	dbParams, err := internal.ResolveParams(input)
+	if err != nil {
+		return
+	}
+
+	query, execArgs := db.tpl.GetSQLContext(sql, dbParams)
+
+	start := time.Now()
+
+	debugPrint(ctx, db.cfg, query, execArgs...)
+	rows, err := db.tx.Query(query, execArgs...)
+	if err != nil {
+		return nil, internal.GetError(err, query, execArgs...)
+	}
+	defer func() {
+		if rows != nil {
+			rows.Close()
+		}
+	}()
+	printSlowQuery(ctx, db.cfg, time.Since(start), query, execArgs...)
+	result, err = callback(rows)
+	return
+}
+
+func (db *xTrans) dbQueryAs(ctx context.Context, sql string, input any, result any, callback internal.DbResolveResultCallback) (err error) {
+	dbParams, err := internal.ResolveParams(input)
+	if err != nil {
+		return
+	}
+
+	query, execArgs := db.tpl.GetSQLContext(sql, dbParams)
+
+	start := time.Now()
+
+	debugPrint(ctx, db.cfg, query, execArgs...)
+	rows, err := db.tx.Query(query, execArgs...)
+	if err != nil {
+		return internal.GetError(err, query, execArgs...)
+	}
+	defer func() {
+		if rows != nil {
+			rows.Close()
+		}
+	}()
+	printSlowQuery(ctx, db.cfg, time.Since(start), query, execArgs...)
+	err = callback(rows, result)
+	return
 }
--- contrib/xdb/xgorm/trans.go
+++ contrib/xdb/xgorm/trans.go
@@ -2,6 +2,7 @@ package xgorm
 
 import (
 	"context"
+	"database/sql"
 
 	"github.com/zhiyunliu/glue/contrib/xdb/internal"
 	"github.com/zhiyunliu/glue/contrib/xdb/tpl"
@@ -25,8 +26,13 @@ func (d *transWrap) Commit() (err error) {
 	return
 }
 
-func (d *transWrap) Query(ctx context.Context, sql string, input map[string]interface{}) (data xdb.Rows, err error) {
-	query, execArgs := d.tpl.GetSQLContext(sql, input)
+func (d *transWrap) Query(ctx context.Context, sql string, input any) (data xdb.Rows, err error) {
+	dbParam, err := internal.ResolveParams(input)
+	if err != nil {
+		return
+	}
+
+	query, execArgs := d.tpl.GetSQLContext(sql, dbParam)
 
 	rows, err := d.gromDB.Raw(query, execArgs...).Rows()
 	if err != nil {
@@ -37,8 +43,13 @@ func (d *transWrap) Query(ctx context.Context, sql string, input map[string]inte
 	return
 
 }
-func (d *transWrap) Multi(ctx context.Context, sql string, input map[string]interface{}) (data []xdb.Rows, err error) {
-	query, execArgs := d.tpl.GetSQLContext(sql, input)
+
+func (d *transWrap) Multi(ctx context.Context, sql string, input any) (data []xdb.Rows, err error) {
+	dbParam, err := internal.ResolveParams(input)
+	if err != nil {
+		return
+	}
+	query, execArgs := d.tpl.GetSQLContext(sql, dbParam)
 
 	rows, err := d.gromDB.Raw(query, execArgs...).Rows()
 	if err != nil {
@@ -48,7 +59,8 @@ func (d *transWrap) Multi(ctx context.Context, sql string, input map[string]inte
 	data, err = internal.ResolveMultiRows(rows)
 	return
 }
-func (d *transWrap) First(ctx context.Context, sql string, input map[string]interface{}) (data xdb.Row, err error) {
+
+func (d *transWrap) First(ctx context.Context, sql string, input any) (data xdb.Row, err error) {
 	rows, err := d.Query(ctx, sql, input)
 	if err != nil {
 		return
@@ -56,7 +68,8 @@ func (d *transWrap) First(ctx context.Context, sql string, input map[string]inte
 	data = rows.Get(0)
 	return
 }
-func (d *transWrap) Scalar(ctx context.Context, sql string, input map[string]interface{}) (data interface{}, err error) {
+
+func (d *transWrap) Scalar(ctx context.Context, sql string, input any) (data interface{}, err error) {
 
 	row, err := d.First(ctx, sql, input)
 	if err != nil {
@@ -68,8 +81,14 @@ func (d *transWrap) Scalar(ctx context.Context, sql string, input map[string]int
 	data, _ = row.Get(row.Keys()[0])
 	return
 }
-func (d *transWrap) Exec(ctx context.Context, sql string, input map[string]interface{}) (r xdb.Result, err error) {
-	query, execArgs := d.tpl.GetSQLContext(sql, input)
+
+func (d *transWrap) Exec(ctx context.Context, sql string, input any) (r xdb.Result, err error) {
+
+	dbParam, err := internal.ResolveParams(input)
+	if err != nil {
+		return
+	}
+	query, execArgs := d.tpl.GetSQLContext(sql, dbParam)
 
 	tmpDb := d.gromDB.Exec(query, execArgs...)
 	err = tmpDb.Error
@@ -80,4 +99,49 @@ func (d *transWrap) Exec(ctx context.Context, sql string, input map[string]inter
 	return &sqlResult{
 		rowsAffected: tmpDb.RowsAffected,
 	}, err
+}
+
+// Query 查询数据
+func (d *transWrap) QueryAs(ctx context.Context, sqls string, input any, results any) (err error) {
+	return d.dbQueryAs(ctx, sqls, input, results, func(r *sql.Rows, a any) error {
+		return internal.ResolveDataResult(r, results)
+	})
+}
+
+func (d *transWrap) FirstAs(ctx context.Context, sqls string, input any, result any) (err error) {
+	return d.dbQueryAs(ctx, sqls, input, result, func(r *sql.Rows, a any) error {
+		return internal.ResolveDataResult(r, result)
+	})
+}
+
+func (db *transWrap) dbQuery(ctx context.Context, sql string, input any, callback internal.DbResolveMapValCallback) (result any, err error) {
+	dbParam, err := internal.ResolveParams(input)
+	if err != nil {
+		return
+	}
+
+	query, execArgs := db.tpl.GetSQLContext(sql, dbParam)
+	rows, err := db.gromDB.Raw(query, execArgs...).Rows()
+	if err != nil {
+		err = internal.GetError(err, query, execArgs...)
+		return
+	}
+	result, err = callback(rows)
+	return
+}
+
+func (db *transWrap) dbQueryAs(ctx context.Context, sql string, input any, result any, callback internal.DbResolveResultCallback) (err error) {
+	dbParam, err := internal.ResolveParams(input)
+	if err != nil {
+		return
+	}
+
+	query, execArgs := db.tpl.GetSQLContext(sql, dbParam)
+	rows, err := db.gromDB.Raw(query, execArgs...).Rows()
+	if err != nil {
+		err = internal.GetError(err, query, execArgs...)
+		return
+	}
+	err = callback(rows, result)
+	return
 }
