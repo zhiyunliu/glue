99b19f2a0396f9fc03e3e71d5d837881603cefae
liujinyin <liujinyin@xunyou.com>
2024-02-29 16:08:51 +0800

    分布式锁增加自动续期

--- dlocker/options.go
+++ dlocker/options.go
@@ -2,12 +2,21 @@ package dlocker
 
 type Options struct {
 	Data string
+	AutoRenewal bool 
 }
 
 type Option func(opts *Options)
 
+//设置数据
 func WithData(data string) Option {
 	return func(opts *Options) {
 		opts.Data = data
+	}
+}
+
+//自动续期
+func WithAutoRenewal() Option {
+	return func(opts *Options) {
+		opts.AutoRenewal = true
 	}
 }
