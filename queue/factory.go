cfc0c18081ed83046a113a17b5017702cf52423e
liujinyin <liujinyin@xunyou.com>
2024-03-02 14:23:50 +0800

    修改queue 发送header 的bug

--- queue/factory.go
+++ queue/factory.go
@@ -26,11 +26,11 @@ func newQueue(proto string, cfg config.Config, opts ...Option) (IQueue, error) {
 
 // Send 发送消息
 func (q *queue) Send(ctx context.Context, key string, value interface{}) error {
-	if msg, ok := value.(Message); ok {
-		return q.q.Push(key, msg)
+	msg, ok := value.(Message)
+	if !ok {
+		msg = NewMsg(value)
 	}
 
-	msg := NewMsg(value)
 	msg.Header()[constants.HeaderSourceIp] = global.LocalIp
 	msg.Header()[constants.HeaderSourceName] = global.AppName
 	return q.q.Push(key, msg)
