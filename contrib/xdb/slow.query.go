5c5f81f505de66e94dc9dd3d10076c002a202d18
liujinyin <liujinyin@xunyou.com>
2024-06-06 16:19:02 +0800

    修改慢sql的内容输出。增加配置名称

--- contrib/xdb/slow.query.go
+++ contrib/xdb/slow.query.go
@@ -2,6 +2,7 @@ package xdb
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/zhiyunliu/glue/contrib/xdb/internal"
@@ -19,5 +20,5 @@ func printSlowQuery(ctx context.Context, setting *Setting, timeRange time.Durati
 	if setting.slowThreshold <= 0 || timeRange < setting.slowThreshold {
 		return
 	}
-	setting.logger.Log(ctx, timeRange.Milliseconds(), query, internal.Unwrap(args...)...)
+	setting.logger.Log(ctx, timeRange.Milliseconds(), fmt.Sprintf("[%s] %s", setting.ConnName, query), internal.Unwrap(args...)...)
 }
