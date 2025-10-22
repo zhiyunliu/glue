22a1cf92d9b153f20c7bc674623b5664353f5e5f
liujinyin <liujinyin@xunyou.com>
2025-10-22 18:12:19 +0800

    xdb 处理not in 和notin 合并

--- contrib/xdb/expression/expression_test.go
+++ contrib/xdb/expression/expression_test.go
@@ -135,7 +135,6 @@ func TestDefaultGetPropName(t *testing.T) {
 		{name: "not-9b.", matcher: likeMatcher, fullKey: "&{tbl.field notlike property%}", wantFullfield: "tbl.field", wantPropName: "property", wantOper: "notlike%", wantSymbol: "&", wantExpr: "and tbl.field not like ?+'%'"},
 		{name: "not-10b.", matcher: likeMatcher, fullKey: "&{tbl.field notlike %property%}", wantFullfield: "tbl.field", wantPropName: "property", wantOper: "%notlike%", wantSymbol: "&", wantExpr: "and tbl.field not like '%'+?+'%'"},
 
-		{name: "notin-8.", matcher: inMatcher, fullKey: "&{notin tbl.infield}", wantFullfield: "tbl.infield", wantPropName: "infield", wantOper: "notin", wantSymbol: "&", wantExpr: "and tbl.infield not in (1,2)"},
 		{name: "8.", matcher: inMatcher, fullKey: "&{in tbl.infield}", wantFullfield: "tbl.infield", wantPropName: "infield", wantOper: "in", wantSymbol: "&", wantExpr: "and tbl.infield in (1,2)"},
 		{name: "9.", matcher: inMatcher, fullKey: "&{in infield}", wantFullfield: "infield", wantPropName: "infield", wantOper: "in", wantSymbol: "&", wantExpr: "and infield in (1,2)"},
 		{name: "f.", matcher: inMatcher, fullKey: "&{in    tbl.infield}", wantFullfield: "tbl.infield", wantPropName: "infield", wantOper: "in", wantSymbol: "&", wantExpr: "and tbl.infield in (1,2)"},
@@ -143,6 +142,11 @@ func TestDefaultGetPropName(t *testing.T) {
 		{name: "h.", matcher: inMatcher, fullKey: "&{infield  in  inproperty}", wantFullfield: "infield", wantPropName: "inproperty", wantOper: "in", wantSymbol: "&", wantExpr: "and infield in ('p1','p2')"},
 		{name: "i.", matcher: inMatcher, fullKey: "&{tt.infield  in    inproperty}", wantFullfield: "tt.infield", wantPropName: "inproperty", wantOper: "in", wantSymbol: "&", wantExpr: "and tt.infield in ('p1','p2')"},
 		{name: "notin-i.", matcher: inMatcher, fullKey: "&{tt.infield  notin    inproperty}", wantFullfield: "tt.infield", wantPropName: "inproperty", wantOper: "notin", wantSymbol: "&", wantExpr: "and tt.infield not in ('p1','p2')"},
+
+		{name: "notin-2.", matcher: inMatcher, fullKey: "&{tt.infield  not in    inproperty}", wantFullfield: "tt.infield", wantPropName: "inproperty", wantOper: "notin", wantSymbol: "&", wantExpr: "and tt.infield not in ('p1','p2')"},
+		{name: "notin-3.", matcher: inMatcher, fullKey: "&{tt.infield  not     in    inproperty}", wantFullfield: "tt.infield", wantPropName: "inproperty", wantOper: "notin", wantSymbol: "&", wantExpr: "and tt.infield not in ('p1','p2')"},
+		{name: "notin-8.", matcher: inMatcher, fullKey: "&{notin tbl.infield}", wantFullfield: "tbl.infield", wantPropName: "infield", wantOper: "notin", wantSymbol: "&", wantExpr: "and tbl.infield not in (1,2)"},
+		{name: "notin-9.", matcher: inMatcher, fullKey: "&{not     in tbl.infield}", wantFullfield: "tbl.infield", wantPropName: "infield", wantOper: "notin", wantSymbol: "&", wantExpr: "and tbl.infield not in (1,2)"},
 
 		{name: "8-a.", matcher: inMatcher, fullKey: "&{in tbl.infield}", wantFullfield: "tbl.infield", wantPropName: "infield", wantOper: "in", wantSymbol: "&", wantExpr: "and tbl.infield in (1,2)"},
 		{name: "9-a.", matcher: inMatcher, fullKey: "&{in infield}", wantFullfield: "infield", wantPropName: "infield", wantOper: "in", wantSymbol: "&", wantExpr: "and infield in (1,2)"},
