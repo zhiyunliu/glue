3b685e59f3f0192818e56578d344aabb8b857dd2
liujinyin <liujinyin@xunyou.com>
2025-11-18 14:45:39 +0800

    增加关闭服务的日志

--- cli/service.stop.go
+++ cli/service.stop.go
@@ -20,15 +20,19 @@ func (p *ServiceApp) stopServers() {
 	log.Infof("serviceApp close:%s stop servers", p.cliCtx.App.Name)
 
 	p.options.StopingHooks.Exec(p.svcCtx, log.DefaultLogger)
-
+	var servers = p.options.Servers
 	for i := range p.options.Servers {
-		if err := p.options.Servers[i].Stop(p.svcCtx); err != nil {
-			log.Errorf("stop server %s error:%s", p.options.Servers[i].Name(), err.Error())
+		stopErr := servers[i].Stop(p.svcCtx)
+		stopMsg := "success"
+		if stopErr != nil {
+			stopMsg = stopErr.Error()
 		}
+		log.Infof("stop server[%s]=%s", servers[i].Name(), stopMsg)
 		p.closeWaitGroup.Done()
 	}
 	p.options.StopedHooks.Exec(p.svcCtx, log.DefaultLogger)
 	p.closeWaitGroup.Wait()
+	log.Infof("serviceApp close:%s stop servers completed", p.cliCtx.App.Name)
 }
 
 func (p *ServiceApp) closeLogger() {
